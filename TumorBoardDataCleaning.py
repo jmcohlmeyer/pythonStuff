09cc89a2f2dc0d8d97ca58c7e27bcfd35a1d91d6
jmcohlmeyer <jamiecohlmeyer@gmail.com>
2016-05-17 11:29:11 -0400

    Data Cleaning utility update

--- TumorBoardDataCleaning.py
+++ TumorBoardDataCleaning.py
@@ -7,8 +7,15 @@ Created on Mon Mar 14 21:15:17 2016
 
 # ---modulate imports---
 import pandas as pd
+#import datetime as dt
+
 #import re
 # ---CSV file import---
+#df1 = pd.read_csv("/Users/jamie/Desktop/UNC Work Files/ReferralandMailComboList.csv")
+#headers  = list(df1)
+#print(df1.head())
+
+
 df = pd.read_csv("/Users/jamie/Desktop/UNC Work Files/GUConferencePrint-1532016.csv")
 
 # ---Column splits---
@@ -17,10 +24,48 @@ pat.columns = ['PATIENT_name', 'PID', 'pAGE']
 
 #print (pat)
 
-md = df.iloc[:, 1].str.split('\n', expand=True)
-md.columns = ['MD', 'MD2', 'MD3', 'MD4', 'MD5','MD6']
+#md = df.iloc[:, 1].str.split('\n', expand=True)
+#md.columns = ['MD', 'MD2', 'MD3', 'MD4', 'MD5','MD6']
 
-#print (md)
+# --- loop for referral date cleaning ----
+referral_d = df.iloc[:,1]
+
+
+#fmts = ('%m/%d/%Y','%m/%d/%y')
+#
+#parsed=[]
+#for e in referral_d.splitlines():
+#    for fmt in fmts:
+#        try:
+#           t = dt.datetime.strptime(e, fmt)
+#           parsed.append((e, fmt, t)) 
+#           break
+#        except ValueError as err:
+#           pass
+#
+## check that all the cases are handled        
+#success={t[0] for t in parsed}
+#for e in referral_d.splitlines():
+#    if e not in success:
+#        print (e)    
+#
+#for t in parsed:
+#    print ('"{:20}" => "{:20}" => {}'.format(*t))
+#
+#
+
+
+
+ref_date = []
+
+#cnt = len(referral_d)
+
+for d in referral_d:   
+        ref_date.append('05/09/2016')
+        
+ref_date_sum = pd.DataFrame(ref_date)
+ref_date_sum.columns = ['Referral Date']    
+print (ref_date_sum)
 
 refmd = df.iloc[:, 2].str.split(',', expand=True)
 refmd.columns = ['Physician Last Name','Physician First Name']
@@ -28,27 +73,11 @@ refmd.columns = ['Physician Last Name','Physician First Name']
 #print (refmd)
 
 diag = df.iloc[:, 3]
-p = 'Prostate'
-b = 'Bladder'
-t = 'Testicular'
-w = 'Penile'
-k = 'Renal'
-u = 'Ureteral'
-kid = 'Kidney'
-o = 'Other'
-uro = 'Urothelial'
-up = 'Upper Tract'
-ur = 'Ureteral'
-s = 'Scrotal'
-ure = 'Ureter'
-te = 'Testis'
-pe = 'Penis'
-
-cancer = [p,b,t,w,k,u,kid,o,uro,up,ur,s,ure,te,pe]
+cancer = ['Prostate','Bladder','Testicular','Penile','Renal','Ureteral','Kidney','Other','Urothelial','Upper Tract','Ureteral','Scrotal','Ureter','Testis','Penis']
 
 # ---Loop for diag simplifcation---
 diag_type = []
-cnt = len(diag)
+
 for string in diag:
     if cancer[0] in string:
         diag_type.append(cancer[0])
@@ -80,6 +109,7 @@ for string in diag:
         diag_type.append(cancer[14]) 
     else:
         diag_type.append(string)
+        
 diag_sum = pd.DataFrame(diag_type)
 diag_sum.columns = ['Cancer Type']
 #print(diag_sum)
@@ -89,7 +119,7 @@ diag_sum.columns = ['Cancer Type']
 #year = 2016
 
 # ---Export reduced data as CSV---
-df_out =  pd.concat([md, diag_sum, refmd], axis=1, join_axes=[pat.index])
+df_out =  pd.concat([ref_date_sum, diag_sum, refmd], axis=1, join_axes=[pat.index])
 df_out.to_csv("/Users/jamie/Desktop/UNC Work Files/test-05122016.csv", encoding='utf-8')
 
         
